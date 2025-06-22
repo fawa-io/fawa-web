aeba60674c291f51f32fd2768c50c3cca726f0b5
whosefriendA <whosefrienda@163.com>
2025-06-22 17:33:43 +0800

    refactor: use pnpm
    Signed-off-by: whosefriendA <whosefrienda@163.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-  }
+  optimizeDeps: {
+    include: [
+      '@bufbuild/protobuf',
+      '@connectrpc/connect',
+      '@connectrpc/connect-web',
+    ],
+  },
+  resolve: {
+    alias: {
+      './gen/proto': path.resolve(__dirname, 'src/gen/proto'),
+    },
+  },
 })
