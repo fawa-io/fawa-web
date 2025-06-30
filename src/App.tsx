ae11aec88929b34c6ce5819b2e4086ee9a4aa449
marsevilspirit <marsevilspirit@gmail.com>
2025-06-30 21:54:54 +0800

    support client stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { FileConsole } from './services/file/components/FileConsole';
+import { GreetConsole } from './services/greet/components/GreetConsole';
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-        <FileConsole />
-      </div>
-  );
+	return (
+		<div className="App">
+			<GreetConsole />
+		</div>
+	);
 }
 
 export default App;
+
+// import { FileConsole } from './services/file/components/FileConsole';
+// import './App.css';
+//
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<FileConsole />
+// 		</div>
+// 	);
+// }
+//
+// export default App;
+//
