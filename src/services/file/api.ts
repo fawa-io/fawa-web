18102507905a60f50d9e951137f1292a5cee4985
marsevilspirit <marsevilspirit@gmail.com>
2025-07-01 19:47:45 +0800

    feat: support client stream (#22)

--- src/services/file/api.ts
+++ src/services/file/api.ts
@@ -1,7 +1,8 @@
-import {createPromiseClient} from "@connectrpc/connect";
-import {FileService} from "../../gen/fawa/file/v1/file_connect.ts";
-import {transport} from "../../common/transport.ts";
+import { createClient } from "@connectrpc/connect";
+import { FileService } from "../../gen/fawa/file/v1/file_pb";
+import { transport } from "../../common/transport.ts";
 
-const client = createPromiseClient(FileService, transport);
+const client = createClient(FileService, transport);
 
-export const fileApiClient = client;
+export const fileClient = client;
+
