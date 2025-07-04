45786ec6e9b2329406d78c163661b752da9c6ef2
whosefriendA <whosefrienda@163.com>
2025-07-04 21:17:04 +0800

    refactor: remove common and use new transport

--- src/services/file/api.ts
+++ src/services/file/api.ts
@@ -3,7 +3,7 @@ import { FileService } from "../../gen/fawa/file/v1/file_pb";
 import { createConnectTransport } from "@connectrpc/connect-web";
 
 const transport = createConnectTransport({
-  baseUrl: import.meta.env.VITE_FILE_SERVICE_URL || 'https://localhost:8080',
+  baseUrl: import.meta.env.VITE_FILE_SERVICE_URL || 'https://3.1949.9.32:8080',
 });
 
 const client = createClient(FileService, transport);
--- src/services/greet/api.ts
+++ src/services/greet/api.ts
@@ -5,7 +5,7 @@ import { GreetService } from "../../gen/fawa/greet/v1/hello_pb";
 import { createConnectTransport } from "@connectrpc/connect-web";
 
 const transport = createConnectTransport({
-  baseUrl: import.meta.env.VITE_GREET_SERVICE_URL || 'https://localhost:8080',
+  baseUrl: import.meta.env.VITE_GREET_SERVICE_URL || 'https://3.149.9.32:8080',
 });
 
 const client = createClient(GreetService, transport);
