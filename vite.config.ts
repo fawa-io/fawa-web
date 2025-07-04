555cfeb1d052695a15d5a01023a7ef8810b09c0f
Ubuntu <ubuntu@ip-172-31-10-22.us-east-2.compute.internal>
2025-07-04 17:28:19 +0000

    feat: cloud serverice all done

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'fawa.marsevilspirit.com',
+    ],
+  },
   optimizeDeps: {
     include: [
       '@bufbuild/protobuf',
