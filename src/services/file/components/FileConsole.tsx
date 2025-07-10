fc1803c2b2ac1c4aa3776dd0c990c7053d2d06b6
whosefriendA <whosefrienda@163.com>
2025-07-10 20:29:02 +0800

    optimize: two dragon function
    Signed-off-by: whosefriendA <whosefrienda@163.com>

--- src/services/file/components/FileConsole.tsx
+++ src/services/file/components/FileConsole.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { useFileService } from '../hooks/useFileService';
 import { Link } from 'react-router-dom';
 import './FileService.css';
